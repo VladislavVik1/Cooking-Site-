7762c46e13966d62979ed1455b1a1054951d9ae3
VladislavVik1 <Dakraman1232@ukr.net>
2025-09-17 20:55:13 +0300

    entrypoint: drop envsubst, use sed

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,10 +4,9 @@ set -e
 : "${PORT:=10000}"
 
 mkdir -p /run/php /run/nginx
-
 mkdir -p /data/uploads
 chown -R www-data:www-data /data/uploads
 
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+sed "s|\${PORT}|${PORT}|g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
